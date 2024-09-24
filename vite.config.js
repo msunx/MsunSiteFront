f96959a4494e0430578a7c00510852e820a06383
慕阳 <xuximing@youzan.com>
2024-09-24 11:20:02 +0800

    init

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,20 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import copy from 'rollup-plugin-copy'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/MsunSiteFront/',
   plugins: [
     vue(),
+    copy({
+      // 显示日志
+      verbose: true,
+      targets: [
+        { src: 'public/404.html', dest: 'dist' }
+      ]
+    })
   ],
   resolve: {
     alias: {
