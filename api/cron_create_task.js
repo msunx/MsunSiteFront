6f10f2db2538632abc9b16a48dc52011b99452e1
慕阳 <xuximing@youzan.com>
2024-10-17 19:24:56 +0800

    init

--- api/cron_create_task.js
+++ api/cron_create_task.js
@@ -1,6 +1,9 @@
-import { NextResponse } from 'next/server';
-
-export async function GET() {
+export default function handler(req, res) {
     console.log('调用了')
-    return NextResponse.json({ ok: true });
+    if (req.headers.get('authorization') !== `bearer ${process.env.cron_secret}`) {
+        console.log('验证没通过')
+        return res.status(401).end('unauthorized');
+    }
+    console.log('验证通过了')
+    res.status(200).end('hello cron!');
 }
