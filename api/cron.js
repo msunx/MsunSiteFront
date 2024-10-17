d6f43a78266c125e977e335473985e7b63c715bd
慕阳 <xuximing@youzan.com>
2024-10-17 19:49:31 +0800

    cron

--- api/cron.js
+++ api/cron.js
@@ -1,9 +1,12 @@
-export default function handler(req, res) {
+export function GET(request) {
     console.log('调用了')
-    if (req.headers.get('authorization') !== `bearer ${process.env.CRON_SECRET}`) {
+    const authHeader = request.headers.get('authorization');
+    if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
         console.log('验证没通过')
-        return res.status(401).end('unauthorized');
+        return new Response('Unauthorized', {
+            status: 401,
+        });
     }
     console.log('验证通过了')
-    res.status(200).end('hello cron!');
+    return Response.json({ success: true });
 }
