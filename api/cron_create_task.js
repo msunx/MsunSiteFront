6e4b486150ea3b24b861b00c1aa1c1b1a7e0b67f
慕阳 <xuximing@youzan.com>
2024-10-18 15:45:12 +0800

    cron

--- api/cron_create_task.js
+++ api/cron_create_task.js
@@ -1,5 +1,7 @@
 import { Client } from '@notionhq/client';
 
+const notion = new Client({ auth: process.env.NOTION_API_KEY });
+
 export async function GET(request) {
     console.log('调用了')
     const authHeader = request.headers.get('authorization');
@@ -14,11 +16,32 @@ export async function GET(request) {
     return Response.json({ success: true });
 }
 
-
-
 async function createTask() {
+    const config = await getConfig()
+    for (const item of config) {
+        if (checkNeedCreateTask(item)) {
+            console.log('需要创建任务')
+            await doCreateTask(item.data.title, item.data.tag)
+        } else {
+            console.log('不需要创建任务')
+        }
+    }
+}
+
+function checkNeedCreateTask(config) {
+    console.log('config', config)
+    if (config.type === 'day') {
+        const now = new Date()
+        const nowStr = (now.getMonth() + 1) + '-' + now.getDate()
+        console.log('nowStr', nowStr)
+        console.log('config.value', config.value)
+        return config.value === nowStr
+    }
+    return false
+}
+
+async function doCreateTask(title, tag) {
     console.log('验证通过了xxxx')
-    const notion = new Client({ auth: process.env.NOTION_API_KEY });
     const response = await notion.pages.create({
         "icon": {
             "type": "emoji",
@@ -33,21 +56,30 @@ async function createTask() {
                 "title": [
                     {
                         "text": {
-                            "content": "测试MX"
+                            "content": title
                         }
                     }
                 ]
             },
             "日期": {
                 "date": {
-                    "start": new Date().toISOString().split('T')[0] 
+                    "start": new Date().toISOString().split('T')[0]
                 }
             },
             "标签": {
                 "select": {
-                    "name": "工作"
+                    "name": tag
                 }
             }
         }
     });
+}
+
+async function getConfig() {
+    const blockId = process.env.CRON_TASK_CONFIG;
+    const response = await notion.blocks.retrieve({
+        block_id: blockId,
+    });
+    console.log(response.code.rich_text[0].plain_text);
+    return JSON.parse(response.code.rich_text[0].plain_text)
 }
